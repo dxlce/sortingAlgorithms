f13cab3ec6ffa97d3df45163a0355357d5b6cc07
xiuxxi <ljin3@ocdsb.ca>
2020-07-08 14:25:04 -0400

    finished shell sort

--- shellSort/shellSort.java
+++ shellSort/shellSort.java
@@ -2,35 +2,51 @@ public class shellSort
 {
     public static void main (String[] args)
     {
+        int a[] = {14, 12, 4, 56, 2, 1};
 
+        sortArray obj = new sortArray();
+        obj.segmentedInsertionSort(a);
+
+        obj.printArray(a);
     }
 }
 
 class sortArray
 {
-    void segmentedInsertionSort(int a[], int k)
+    void segmentedInsertionSort(int a[])
     {
-        int j, m, n;
-        boolean done;
 
-        n = a.length;
+
+        int n = a.length;
 
         //Insert element from index m in approrpiate slot in its segment
 
-        for (m = k; m < n; ++m)
+        for (int gap = n/2; gap > 0; gap /= 2)
         {
-            j = m;
-            done = false;
-
-            //when j-kth element precedes jth element, jth is appropriately positioned in its segment
-
-            while ((j >= k) && !done)
+            for (int i = gap; i < n; i+=1)
             {
-                if (a[j] < a[j-k])
+                int temp = a[i];
+
+                int j;
+                for (j = i; j >= gap && a[j-gap] > temp; j-= gap)
                 {
-                
+                    a[j] = a[j-gap];
                 }
+
+                a[j] = temp;
             }
         }
     }
+
+    void printArray(int a[])
+    {
+        int n = a.length;
+
+        for (int i = 0; i < n; i++)
+        {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
 }
