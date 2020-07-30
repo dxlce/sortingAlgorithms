56b86d0b4fafb16a09d7e7090682f5e16c43cda0
xiuxxi <ljin3@ocdsb.ca>
2020-07-30 16:37:58 -0400

    added comments

--- list.cpp
+++ list.cpp
@@ -57,26 +57,42 @@ void List<BaseData>::insertBefore(const BaseData &item)
     ListNode<BaseData> *p;
     p = new ListNode<BaseData>;
 
-    //
+    //assign contents of item to the node referenced by p
     p->listData = item;
+
+    //if the current position of node is less than or equal to one (ie. inserting node at beginning of list)
     if (currentPos <= 1)
     {
+        //assign head to p
         head = p;
+        
+        //current position is now one
         currentPos = 1;
+
+        //if the number of nodes is zero (ie. the only node in the list is 'item') - link it to null
         if (numnodes == 0)
         {
             p->link = null;
         }
+
+        //else: link p to currentNode
         else
         {
             p->link = currentNode;
         } 
     }
+
+    //else
     else
     {
+        //link p to current node
         p->link = currentNode;
+        
+        //link previous node to p
         previous->link = p;
     }
+
+    //increase numNodes by one
     ++numNodes;
     currentNode = p;
     
